1bd0ad5a467f22a88d9dac28876ba3fbd3ffc3ec
JefferyXie <jex862@usask.ca>
2021-12-20 17:59:52 -0600

    Cleaned up App component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Projects from './Components/Projects/Projects';
+import About from './Components/About/About';
+import Skills from './Components/Skills/Skills';
+import Contact from './Components/Contact/Contact';
+
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Projects></Projects>
+      <About></About>
+      <Skills></Skills>
+      <Contact></Contact>
     </div>
   );
 }
