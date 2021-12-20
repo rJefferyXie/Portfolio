3a1cef84311b29e8f8526ab7cea6c05357254500
JefferyXie <jex862@usask.ca>
2021-12-20 17:59:01 -0600

    Created Projects component.

---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import './Projects.css';
+
+const Projects = () => {
+    return (
+        <div className="Projects">
+            <p>Projects</p>
+        </div>
+    )
+}
+
+export default Projects;
