955c251eff8f2aa5c19fa0e3ef3921e95f4bf5dd
JefferyXie <jex862@usask.ca>
2021-12-20 23:34:47 -0600

    Added hero text.

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,6 +3,15 @@ import './Hero.css';
 const Hero = () => {
     return (
         <div className="Hero">
+            <div className="Intro-container flex">
+                <div className="Text-line flex">
+                    <p>Hello! My name is 
+                        <span> Jeffery Xie </span> 
+                        and I am a 
+                        <span> Front End Developer.</span>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
