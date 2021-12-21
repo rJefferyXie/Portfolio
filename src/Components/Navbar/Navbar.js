6faf411c637377b6f82a74240f16c6a345c5d7a3
JefferyXie <jex862@usask.ca>
2021-12-20 23:34:37 -0600

    Added mobile navbar.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,29 @@
 import './Navbar.css';
 
 const Navbar = () => {
+    const menu = document.querySelector(".Mobile-menu");
+    const menuLinks = document.querySelector('.Navbar-container');
+
+    // Display Mobile Menu
+    const mobileMenu = () => {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    };
+
     return (
         <nav className="Navbar flex">
             <button className="Navbar-logo">Jeffery Xie</button>
-            <div className="Navbar-container flex">
+            <div className="Mobile-menu" onClick={mobileMenu}>
+                <span className="bar"></span> 
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+            <div className="Navbar-container flex" onClick={mobileMenu}>
                 <button className="Navbar-button">About</button>
                 <button className="Navbar-button">Projects</button>
                 <button className="Navbar-button">Skills</button>
                 <button className="Navbar-button">Contact</button>
+                <button className="Navbar-button Resume">Resume</button>
             </div>
         </nav>
     );
