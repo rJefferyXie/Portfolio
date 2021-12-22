7e6df5f4cd7414b0a4543f5a16ae147db3e25d9f
JefferyXie <jex862@usask.ca>
2021-12-22 17:24:44 -0600

    Added animated down arrow.

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,15 +1,29 @@
 import './Hero.css';
 
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Hero = () => {
+    const ScrollTo = (id) => {
+        let section = document.querySelector(id);
+        section.scrollIntoView({ 
+            behavior: "smooth",
+            block: "center"
+        });
+    }
+
     return (
-        <div className="Hero">
-            <div className="Intro-container flex">
+        <div id="Hero">
+            <div className="Intro-container flex-col">
                 <div className="Text-line flex">
                     <p>Hello! My name is 
                         <span> Jeffery Xie </span> 
                         and I am a 
-                        <span> Front End Developer.</span>
+                        <span> Front-End Developer.</span>
                     </p>
+                </div>
+                <div className="Arrow-circle flex">
+                    <FontAwesomeIcon icon={faArrowDown} className="Down-arrow" onClick={() => ScrollTo("#Projects")}></FontAwesomeIcon>
                 </div>
             </div>
         </div>
