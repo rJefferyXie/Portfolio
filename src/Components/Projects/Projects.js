2e7696b5fe773dc3e645e8e921a375673dd779fb
JefferyXie <jex862@usask.ca>
2022-01-01 01:02:57 -0600

    Added two more projects.

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,6 +4,8 @@ import unite1 from "../../images/unite-1.png";
 import pathfinder1 from "../../images/pathfinder-2.png";
 import pokepedia1 from "../../images/pokepedia-1.png";
 import catstronauts1 from "../../images/catstronauts-1.png";
+import chess1 from "../../images/chess-1.png";
+import webscraper1 from "../../images/web-scraper-2.png";
 
 import { faReact } from '@fortawesome/free-brands-svg-icons';
 import { faCss3 } from '@fortawesome/free-brands-svg-icons';
@@ -27,7 +29,7 @@ const Projects = () => {
                 <div className="Project-card flex-col">
                     <img src={unite1} alt="" className="Project-image"></img>
                     <div className="Project-header flex">
-                        <h1 className="Project-name">unitetheworld</h1>
+                        <h1 className="Project-name">UniteTheWorld</h1>
                         <div className="Technologies flex">
                             <div className="Tech flex-col">
                                 <FontAwesomeIcon icon={faLeaf}></FontAwesomeIcon>
@@ -48,9 +50,9 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="Project-info">
-                        <p className="Project-description">A community-based organization that focuses on giving food to the people that need it the most. Includes user authentication and verification, all built with the MERN stack.</p>
+                        <p className="Project-description">A community-based organization that focuses on giving food to the people that need it the most. Includes user authentication and verification.</p>
                         <div className="Links flex">
-                            <a href="https://unitetheworld.netlify.app/" target="_blank" rel="noreferrer" className="Project-link">Live Demo</a>
+                            <a href="https://unitetheworld.netlify.app/" target="_blank" rel="noreferrer" className="Project-link">Demo</a>
                             <a href="https://github.com/rJefferyXie/unitetheworld" target="_blank" rel="noreferrer" className="Project-link">Code</a>
                         </div>
                     </div>
@@ -79,7 +81,7 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="Project-info">
-                        <p className="Project-description">A complete pokedex that includes information about every pokemon, a music player, and a built-in team generator. This website utilizes PokéAPI for the data.</p>
+                        <p className="Project-description">A complete pokedex that includes information about every pokemon, a music player, and a built-in team generator. This website uses PokéAPI for the data.</p>
                         <div className="Links flex">
                             <a href="https://rjefferyxie.github.io/Pokepedia/" target="_blank" rel="noreferrer" className="Project-link">Demo</a>
                             <a href="https://github.com/rJefferyXie/Pokepedia" target="_blank" rel="noreferrer" className="Project-link">Code</a>
@@ -106,9 +108,27 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="Project-info">
-                        <p className="Project-description">An arcade-like shooter game built with Unity. Travel to new planets in space to rescue your animal friends. I created this game with five other people in my CMPT 306 course.</p>
+                        <p className="Project-description">An arcade-like shooter game built with Unity. Travel to new planets in space to rescue your animal friends. I created this game with five other people.</p>
                         <div className="Links flex">
                             <a href="https://drive.google.com/file/d/1rnp4R0C5zJJRnaV1ghvS4k04SSIli_YM/view?usp=sharing" target="_blank" rel="noreferrer" className="Project-link">Demo</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="Project-card flex">
+                    <img src={webscraper1} alt="" className="Project-image"></img>
+                    <div className="Project-header flex">
+                        <h1 className="Project-name">Clothing Web Scraper</h1>
+                        <div className="Technologies flex">
+                            <div className="Tech flex-col">
+                                <FontAwesomeIcon icon={faPython}></FontAwesomeIcon>
+                                <p className="Tech-name">Python</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Project-info">
+                        <p className="Project-description">This app is a one-stop shop that gathers data from popular clothing sites and presents the clothing items to its users in an easily readable format.</p>
+                        <div className="Links flex">
+                            <a href="https://github.com/rJefferyXie/Pathfinding-Visualizer" target="_blank" rel="noreferrer" className="Project-link">Code</a>
                         </div>
                     </div>
                 </div>
@@ -124,9 +144,27 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="Project-info">
-                        <p className="Project-description">A visual pathfinding program that allows the user to create their own obstacles or mazes and then run different pathfinding algorithms on it. Built with pygame.</p>
+                        <p className="Project-description">A visual pathfinding program that allows the user to create their own obstacles or mazes and then run different pathfinding algorithms on it.</p>
                         <div className="Links flex">
                             <a href="https://github.com/rJefferyXie/Pathfinding-Visualizer" target="_blank" rel="noreferrer" className="Project-link">Code</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="Project-card flex">
+                    <img src={chess1} alt="" className="Project-image"></img>
+                    <div className="Project-header flex">
+                        <h1 className="Project-name">Chess Engine Visualizer</h1>
+                        <div className="Technologies flex">
+                            <div className="Tech flex-col">
+                                <FontAwesomeIcon icon={faPython}></FontAwesomeIcon>
+                                <p className="Tech-name">Python</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Project-info">
+                        <p className="Project-description">This program includes local multiplayer and single player. The AI uses the minimax algorithm, and you can watch it decide on its moves in real time. </p>
+                        <div className="Links flex">
+                            <a href="https://github.com/rJefferyXie/Chess-Program-with-Minimax-Visualizer" target="_blank" rel="noreferrer" className="Project-link">Code</a>
                         </div>
                     </div>
                 </div>
