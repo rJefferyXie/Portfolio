96c7592046e5b0ba6a760db30d54a9ccf5222adf
JefferyXie <jex862@usask.ca>
2022-01-02 01:11:02 -0600

    Created footer for credit information.

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <section id="Footer" className="flex">
+            <div className="Page-container">
+                <p>Technology icons from  <a href="https://icons8.com/icons/">https://icons8.com/icons/</a>
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Footer;
