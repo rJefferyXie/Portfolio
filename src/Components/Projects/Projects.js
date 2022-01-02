f5614e0098016acd46e23b8ccf891522f69623f9
JefferyXie <jex862@usask.ca>
2022-01-02 01:11:57 -0600

    Worked on resizing projects.

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,49 +7,61 @@ import catstronauts1 from "../../images/catstronauts-1.png";
 import chess1 from "../../images/chess-1.png";
 import webscraper1 from "../../images/web-scraper-2.png";
 
-import { faReact } from '@fortawesome/free-brands-svg-icons';
-import { faCss3 } from '@fortawesome/free-brands-svg-icons';
-import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
-import { faJs } from '@fortawesome/free-brands-svg-icons';
-import { faServer } from '@fortawesome/free-solid-svg-icons';
-import { faNodeJs } from '@fortawesome/free-brands-svg-icons';
-import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
-import { faLeaf } from '@fortawesome/free-solid-svg-icons';
-import { faUnity } from '@fortawesome/free-brands-svg-icons';
-import { faGitlab } from '@fortawesome/free-brands-svg-icons';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
-import { faPython } from '@fortawesome/free-brands-svg-icons';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// Icons
+import mongodb from "../../images/icons/mongodb.svg";
+import express from "../../images/icons/express.png";
+import react from "../../images/icons/react.svg";
+import nodejs from "../../images/icons/nodejs.svg";
+import javascript from "../../images/icons/javascript.svg";
+import html5 from "../../images/icons/html5.svg";
+import css3 from "../../images/icons/css3.svg";
+import csharp from "../../images/icons/csharp.svg";
+import python from "../../images/icons/python.svg";
+import unity from "../../images/icons/unity.svg";
+import gitlab from "../../images/icons/gitlab.svg";
 
 const Projects = () => {
+    const HoverEffect = () => {
+        let headers = document.getElementsByClassName("Project-header");
+        let info = document.getElementsByClassName("Project-info")
+        for (var i = 0; i < headers.length; i++) {
+            headers[i].classList.toggle("active-header");
+            headers[i].classList.toggle("translate-up");
+            info[i].classList.toggle("active-info");
+        };
+    };
+
     return (
-        <div id="Projects" className="flex">
+        <section id="Projects" className="flex-col">
+            <div className="flex">
+                <h1 className="Page-title">My Projects</h1>
+                <button onClick={HoverEffect}>Hover Effects</button>
+            </div>
             <div id="Project-page-container" className="Page-container flex">
                 <div className="Project-card flex-col">
                     <img src={unite1} alt="" className="Project-image"></img>
-                    <div className="Project-header flex">
+                    <div className="Project-header active-header flex">
                         <h1 className="Project-name">UniteTheWorld</h1>
                         <div className="Technologies flex">
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faLeaf}></FontAwesomeIcon>
+                                <img src={mongodb} alt="" className="icon"></img>
                                 <p className="Tech-name">MongoDB</p>
                             </div>
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faServer}></FontAwesomeIcon>
+                                <img src={express} alt="" className="icon"></img>
                                 <p className="Tech-name">Express</p>
                             </div>
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faReact}></FontAwesomeIcon>
+                                <img src={react} alt="" className="icon"></img>
                                 <p className="Tech-name">React</p>
                             </div>
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faNodeJs}></FontAwesomeIcon>
+                                <img src={nodejs} alt="" className="icon"></img>
                                 <p className="Tech-name">NodeJS</p>
                             </div>
                         </div>
                     </div>
-                    <div className="Project-info">
+                    <div className="Project-info active-info">
                         <p className="Project-description">A community-based organization that focuses on giving food to the people that need it the most. Includes user authentication and verification.</p>
                         <div className="Links flex">
                             <a href="https://unitetheworld.netlify.app/" target="_blank" rel="noreferrer" className="Project-link">Demo</a>
@@ -59,28 +71,24 @@ const Projects = () => {
                 </div>
                 <div className="Project-card flex">
                     <img src={pokepedia1} alt="" className="Project-image"></img>
-                    <div className="Project-header flex">
+                    <div className="Project-header active-header flex">
                         <h1 className="Project-name">Poképedia</h1>
                         <div className="Technologies flex">
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faJs}></FontAwesomeIcon>
+                                <img src={javascript} alt="" className="icon"></img>
                                 <p className="Tech-name">JavaScript</p>
                             </div>
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon>
+                                <img src={html5} alt="" className="icon"></img>
                                 <p className="Tech-name">HTML</p>
                             </div>
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faCss3}></FontAwesomeIcon>
+                                <img src={css3} alt="" className="icon"></img>
                                 <p className="Tech-name">CSS</p>
-                            </div>
-                            <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faCodeBranch}></FontAwesomeIcon>
-                                <p className="Tech-name">PokéAPI</p>
                             </div>
                         </div>
                     </div>
-                    <div className="Project-info">
+                    <div className="Project-info active-info">
                         <p className="Project-description">A complete pokedex that includes information about every pokemon, a music player, and a built-in team generator. This website uses PokéAPI for the data.</p>
                         <div className="Links flex">
                             <a href="https://rjefferyxie.github.io/Pokepedia/" target="_blank" rel="noreferrer" className="Project-link">Demo</a>
@@ -90,24 +98,24 @@ const Projects = () => {
                 </div>
                 <div className="Project-card flex">
                 <img src={catstronauts1} alt="" className="Project-image"></img>
-                    <div className="Project-header flex">
+                    <div className="Project-header active-header flex">
                         <h1 className="Project-name">Catstronauts</h1>
                         <div className="Technologies flex">
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faUnity}></FontAwesomeIcon>
+                                <img src={unity} alt="" className="icon"></img>
                                 <p className="Tech-name">Unity</p>
                             </div>
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faGitlab}></FontAwesomeIcon>
+                                <img src={gitlab} alt="" className="icon"></img>
                                 <p className="Tech-name">Gitlab</p>
                             </div>
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faCode}></FontAwesomeIcon>
+                                <img src={csharp} alt="" className="icon"></img>
                                 <p className="Tech-name">C#</p>
                             </div>
                         </div>
                     </div>
-                    <div className="Project-info">
+                    <div className="Project-info active-info">
                         <p className="Project-description">An arcade-like shooter game built with Unity. Travel to new planets in space to rescue your animal friends. I created this game with five other people.</p>
                         <div className="Links flex">
                             <a href="https://drive.google.com/file/d/1rnp4R0C5zJJRnaV1ghvS4k04SSIli_YM/view?usp=sharing" target="_blank" rel="noreferrer" className="Project-link">Demo</a>
@@ -116,16 +124,16 @@ const Projects = () => {
                 </div>
                 <div className="Project-card flex">
                     <img src={webscraper1} alt="" className="Project-image"></img>
-                    <div className="Project-header flex">
+                    <div className="Project-header active-header flex">
                         <h1 className="Project-name">Clothing Web Scraper</h1>
                         <div className="Technologies flex">
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faPython}></FontAwesomeIcon>
+                                <img src={python} alt="" className="icon"></img>
                                 <p className="Tech-name">Python</p>
                             </div>
                         </div>
                     </div>
-                    <div className="Project-info">
+                    <div className="Project-info active-info">
                         <p className="Project-description">This app is a one-stop shop that gathers data from popular clothing sites and presents the clothing items to its users in an easily readable format.</p>
                         <div className="Links flex">
                             <a href="https://github.com/rJefferyXie/Pathfinding-Visualizer" target="_blank" rel="noreferrer" className="Project-link">Code</a>
@@ -134,16 +142,16 @@ const Projects = () => {
                 </div>
                 <div className="Project-card flex">
                     <img src={pathfinder1} alt="" className="Project-image"></img>
-                    <div className="Project-header flex">
+                    <div className="Project-header active-header flex">
                         <h1 className="Project-name">Pathfinding Visualizer</h1>
                         <div className="Technologies flex">
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faPython}></FontAwesomeIcon>
+                                <img src={python} alt="" className="icon"></img>
                                 <p className="Tech-name">Python</p>
                             </div>
                         </div>
                     </div>
-                    <div className="Project-info">
+                    <div className="Project-info active-info">
                         <p className="Project-description">A visual pathfinding program that allows the user to create their own obstacles or mazes and then run different pathfinding algorithms on it.</p>
                         <div className="Links flex">
                             <a href="https://github.com/rJefferyXie/Pathfinding-Visualizer" target="_blank" rel="noreferrer" className="Project-link">Code</a>
@@ -152,16 +160,16 @@ const Projects = () => {
                 </div>
                 <div className="Project-card flex">
                     <img src={chess1} alt="" className="Project-image"></img>
-                    <div className="Project-header flex">
+                    <div className="Project-header active-header flex">
                         <h1 className="Project-name">Chess Engine Visualizer</h1>
                         <div className="Technologies flex">
                             <div className="Tech flex-col">
-                                <FontAwesomeIcon icon={faPython}></FontAwesomeIcon>
+                                <img src={python} alt="" className="icon"></img>
                                 <p className="Tech-name">Python</p>
                             </div>
                         </div>
                     </div>
-                    <div className="Project-info">
+                    <div className="Project-info active-info">
                         <p className="Project-description">This program includes local multiplayer and single player. The AI uses the minimax algorithm, and you can watch it decide on its moves in real time. </p>
                         <div className="Links flex">
                             <a href="https://github.com/rJefferyXie/Chess-Program-with-Minimax-Visualizer" target="_blank" rel="noreferrer" className="Project-link">Code</a>
@@ -169,7 +177,7 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
