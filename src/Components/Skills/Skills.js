8189892594b391b0ca216f40d14e2997c58d2919
JefferyXie <jex862@usask.ca>
2022-01-02 01:11:50 -0600

    Added technologies to skills.

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,12 +1,64 @@
 import './Skills.css';
 
+// Icons
+import mongodb from "../../images/icons/mongodb.svg";
+import express from "../../images/icons/express.png";
+import react from "../../images/icons/react.svg";
+import nodejs from "../../images/icons/nodejs.svg";
+import javascript from "../../images/icons/javascript.svg";
+import html5 from "../../images/icons/html5.svg";
+import css3 from "../../images/icons/css3.svg";
+import python from "../../images/icons/python.svg";
+import java from "../../images/icons/java.svg";
+import github from "../../images/icons/github.svg";
+
 const Skills = () => {
     return (
-        <div id="Skills" className="flex">
-            <div className="Page-container">
-                <p>Skills</p>
+        <section id="Skills" className="flex-col">
+            <h1 className="Page-title">Skills and Technologies</h1>
+            <div className="Page-container flex">
+                <div className="Tech flex-col">
+                    <img src={mongodb} alt="" className="icon"></img>
+                    <p className="Tech-name">MongoDB</p>
+                </div>
+                <div className="Tech flex-col">
+                    <img src={express} alt="" className="icon"></img>
+                    <p className="Tech-name">Express</p>
+                </div>
+                <div className="Tech flex-col">
+                    <img src={react} alt="" className="icon"></img>
+                    <p className="Tech-name">React</p>
+                </div>
+                <div className="Tech flex-col">
+                    <img src={nodejs} alt="" className="icon"></img>
+                    <p className="Tech-name">NodeJS</p>
+                </div> 
+                <div className="Tech flex-col">
+                    <img src={javascript} alt="" className="icon"></img>
+                    <p className="Tech-name">JavaScript</p>
+                </div>
+                <div className="Tech flex-col">
+                    <img src={html5} alt="" className="icon"></img>
+                    <p className="Tech-name">HTML</p>
+                </div>
+                <div className="Tech flex-col">
+                    <img src={css3} alt="" className="icon"></img>
+                    <p className="Tech-name">CSS</p>
+                </div>         
+                <div className="Tech flex-col">
+                    <img src={python} alt="" className="icon"></img>
+                    <p className="Tech-name">Python</p>
+                </div>
+                <div className="Tech flex-col">
+                    <img src={java} alt="" className="icon"></img>
+                    <p className="Tech-name">Java</p>
+                </div>
+                <div className="Tech flex-col">
+                    <img src={github} alt="" className="icon"></img>
+                    <p className="Tech-name">Git</p>
+                </div>   
             </div>
-        </div>
+        </section>
     )
 }
 
