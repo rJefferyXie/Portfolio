04b9ca9a25eccbc3702a32be003bdedd29463cc5
JefferyXie <jex862@usask.ca>
2022-01-09 00:35:36 -0600

    Removed hero wallpaper, added svg.

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,6 +3,9 @@ import './Hero.css';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import hero from "../../images/hero.svg";
+import Resume from "../../images/Resume.pdf";
+
 const Hero = () => {
     const ScrollTo = (id) => {
         let section = document.querySelector(id).offsetTop - 80;
@@ -13,16 +16,18 @@ const Hero = () => {
     }
 
     return (
-        <section id="Hero">
-            <div className="Intro-container flex-col">
-                <div className="Text-line flex">
+        <section id="Hero" className="flex">
+            <div className="Intro-container flex">
+                <div className="Intro-left flex-col">
                     <p className="Shadow-p Hero-text">Hello! My name is 
                         <span className="Shadow-span Hero-text"> Jeffery Xie </span> 
                         and I am a 
-                        <span className="Shadow-span Hero-text"> Front-End Developer.</span>
+                        <span className="Shadow-span Hero-text"> Front-End Engineer.</span>
                     </p>
+                    <a href={Resume} target="_blank" rel="noreferrer" id="Resume" className="Navbar-button flex">Resume</a>
                 </div>
-                <div className="Arrow-circle flex">
+                <img id="Hero-image" src={hero}></img>
+                <div className="Arrow-container flex">
                     <FontAwesomeIcon icon={faArrowDown} className="Down-arrow" onClick={() => ScrollTo("#Projects")}></FontAwesomeIcon>
                 </div>
             </div>
