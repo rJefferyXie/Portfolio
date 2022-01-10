dfa5f299c172072c7e2bb9905ff82a00a60ec653
JefferyXie <jex862@usask.ca>
2022-01-10 00:11:01 -0600

    Removed wallpaper, added svg image to hero.

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,9 +1,9 @@
-import './Hero.css';
+import "./Hero.css";
 
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import hero from "../../images/hero.svg";
+import hero from "../../images/undraw/hero.svg";
 import Resume from "../../images/Resume.pdf";
 
 const Hero = () => {
@@ -19,12 +19,14 @@ const Hero = () => {
         <section id="Hero" className="flex">
             <div className="Intro-container flex">
                 <div className="Intro-left flex-col">
-                    <p className="Shadow-p Hero-text">Hello! My name is 
-                        <span className="Shadow-span Hero-text"> Jeffery Xie </span> 
-                        and I am a 
-                        <span className="Shadow-span Hero-text"> Front-End Web Developer.</span>
+                    <p id="Hero-header" className="hero-p">Hello there! My name is 
+                        <span className="Shadow-span Hero-text"> Jeffery Xie. </span>
                     </p>
-                    <a href={Resume} target="_blank" rel="noreferrer" id="Resume" className="Navbar-button flex">Resume</a>
+                    <p className="hero-p">I am a 
+                        <span className="Shadow-span Hero-text"> Front-End Web Developer </span>
+                        who is passionate about creating interactive web applications and positive user experiences.
+                    </p>
+                    <a href={Resume} target="_blank" rel="noreferrer" id="Resume" className="nav-button flex">Resume</a>
                 </div>
                 <img id="Hero-image" alt="" src={hero}></img>
                 <div className="Arrow-container flex">
