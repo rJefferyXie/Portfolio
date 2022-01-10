fc6f3a90535c955ea0b365d870e30a2ebe5aaa9f
JefferyXie <jex862@usask.ca>
2022-01-10 00:11:40 -0600

    Renamed some skill tags, responsive friendly.

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -17,56 +17,56 @@ import vscode from "../../images/icons/vscode.svg";
 const Skills = () => {
     return (
         <section id="Skills" className="flex-col">
-            <h1 className="Page-title">Skills and Technologies</h1>
+            <h1>Skills and Technologies</h1>
             <div id="Skills-container" className="flex">
-                <div id="Skill-grid" className="Page-container">
+                <div id="Skill-grid" className="grid page-container">
                     <div className="skill-card flex-col">
                         <img src={mongodb} alt="MongoDB" className="skill-icon"></img>
-                        <p className="skill-name">MongoDB</p>
+                        <p className="tech-name">MongoDB</p>
                     </div>
                     <div className="skill-card flex-col">
                         <img src={express} alt="Express" className="skill-icon"></img>
-                        <p className="skill-name">Express</p>
+                        <p className="tech-name">Express</p>
                     </div>
                     <div className="skill-card flex-col">
                         <img src={react} alt="React" className="skill-icon"></img>
-                        <p className="skill-name">React</p>
+                        <p className="tech-name">React</p>
                     </div>
                     <div className="skill-card flex-col">
                         <img src={nodejs} alt="Node" className="skill-icon"></img>
-                        <p className="skill-name">NodeJS</p>
+                        <p className="tech-name">NodeJS</p>
                     </div> 
                     <div className="skill-card flex-col">
                         <img src={api} alt="Rest APIs" className="skill-icon"></img>
-                        <p className="skill-name">Rest APIs</p>
+                        <p className="tech-name">Rest APIs</p>
                     </div> 
                     <div className="skill-card flex-col">
                         <img src={git} alt="Git" className="skill-icon"></img>
-                        <p className="skill-name">Git</p>
+                        <p className="tech-name">Git</p>
                     </div>  
                     <div className="skill-card flex-col">
                         <img src={javascript} alt="JavaScript" className="skill-icon"></img>
-                        <p className="skill-name">JavaScript</p>
+                        <p className="tech-name">JavaScript</p>
                     </div>
                     <div className="skill-card flex-col">
                         <img src={html5} alt="HTML" className="skill-icon"></img>
-                        <p className="skill-name">HTML</p>
+                        <p className="tech-name">HTML</p>
                     </div>
                     <div className="skill-card flex-col">
                         <img src={css3} alt="CSS" className="skill-icon"></img>
-                        <p className="skill-name">CSS</p>
+                        <p className="tech-name">CSS</p>
                     </div>         
                     <div className="skill-card flex-col">
                         <img src={vscode} alt="VS Code" className="skill-icon"></img>
-                        <p className="skill-name">VSCode</p>
+                        <p className="tech-name">VSCode</p>
                     </div>
                     <div className="skill-card flex-col">
                         <img src={python} alt="Python" className="skill-icon"></img>
-                        <p className="skill-name">Python</p>
+                        <p className="tech-name">Python</p>
                     </div>
                     <div className="skill-card flex-col">
                         <img src={java} alt="Java" className="skill-icon"></img>
-                        <p className="skill-name">Java</p>
+                        <p className="tech-name">Java</p>
                     </div> 
                 </div>
             </div>
