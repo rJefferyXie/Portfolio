8b2ca40fccd1a15747c0e4939122fb00468d9630
JefferyXie <jex862@usask.ca>
2022-01-10 00:10:11 -0600

    Completed contact page with form.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,9 @@
 import './Contact.css';
 
+import ContactForm from './ContactForm/ContactForm';
+
+import email from "../../images/undraw/email.svg";
+
 const Contact = () => {
     const sendEmail = () => {
         window.open('mailto:jeffxie27@outlook.com');
@@ -7,9 +11,14 @@ const Contact = () => {
 
     return (
         <section id="Contact" className="flex-col">
-            <h1 className="Page-title">Get In Touch</h1>
-            <div id="Contact-container" className="Page-container flex-col">
-                <button onClick={sendEmail}>Send Email</button>
+            <h1>Contact</h1>
+            <div id="Contact-container" className="page-container flex">
+            <h2 id="Contact-header">Get In Touch</h2>
+                <div id="Contact-left" className="flex-col">
+                    <img id="Email-image" src={email} alt="Click this to send an email." onClick={sendEmail}></img>
+                    <button id="Send-email" onClick={sendEmail}>SEND EMAIL</button>
+                </div>
+                <ContactForm></ContactForm>
             </div>
         </section>
     )
