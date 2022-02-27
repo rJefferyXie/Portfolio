fcc6c9c48c294183919c3fec04d8d214c3fb2077
JefferyXie <jeffxie27@outlook.com>
2022-02-27 00:08:03 -0600

    Implemented skill cards using MateriallUI.

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -14,60 +14,62 @@ import api from "../../images/icons/api.svg";
 import git from "../../images/icons/git.svg";
 import vscode from "../../images/icons/vscode.svg";
 
+import { Card } from "@mui/material";
+
 const Skills = () => {
     return (
         <section id="Skills" className="flex-col">
             <h1>Skills and Technologies</h1>
             <div id="Skills-container" className="flex">
                 <div id="Skill-grid" className="grid page-container">
-                    <div className="skill-card flex-col">
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={mongodb} alt="MongoDB" className="skill-icon"></img>
                         <p className="tech-name">MongoDB</p>
-                    </div>
-                    <div className="skill-card flex-col">
+                    </Card>
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={express} alt="Express" className="skill-icon"></img>
                         <p className="tech-name">Express</p>
-                    </div>
-                    <div className="skill-card flex-col">
+                    </Card>
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={react} alt="React" className="skill-icon"></img>
                         <p className="tech-name">React</p>
-                    </div>
-                    <div className="skill-card flex-col">
+                    </Card>
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={nodejs} alt="Node" className="skill-icon"></img>
                         <p className="tech-name">NodeJS</p>
-                    </div> 
-                    <div className="skill-card flex-col">
+                    </Card> 
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={api} alt="Rest APIs" className="skill-icon"></img>
                         <p className="tech-name">Rest APIs</p>
-                    </div> 
-                    <div className="skill-card flex-col">
+                    </Card> 
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={git} alt="Git" className="skill-icon"></img>
                         <p className="tech-name">Git</p>
-                    </div>  
-                    <div className="skill-card flex-col">
+                    </Card>  
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={javascript} alt="JavaScript" className="skill-icon"></img>
                         <p className="tech-name">JavaScript</p>
-                    </div>
-                    <div className="skill-card flex-col">
+                    </Card>
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={html5} alt="HTML" className="skill-icon"></img>
                         <p className="tech-name">HTML</p>
-                    </div>
-                    <div className="skill-card flex-col">
+                    </Card>
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={css3} alt="CSS" className="skill-icon"></img>
                         <p className="tech-name">CSS</p>
-                    </div>         
-                    <div className="skill-card flex-col">
+                    </Card>         
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={vscode} alt="VS Code" className="skill-icon"></img>
                         <p className="tech-name">VSCode</p>
-                    </div>
-                    <div className="skill-card flex-col">
+                    </Card>
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={python} alt="Python" className="skill-icon"></img>
                         <p className="tech-name">Python</p>
-                    </div>
-                    <div className="skill-card flex-col">
+                    </Card>
+                    <Card variant="outlined" className="skill-card flex-col">
                         <img src={java} alt="Java" className="skill-icon"></img>
                         <p className="tech-name">Java</p>
-                    </div> 
+                    </Card> 
                 </div>
             </div>
         </section>
